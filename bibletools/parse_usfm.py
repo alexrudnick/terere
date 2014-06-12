0378a6faf2f1da1d5f4b6620d978f30c96e40e0a
Alex Rudnick <alex.rudnick@gmail.com>
2014-06-12 15:27:31 -0400

    remove cross-references and subheaders, restructure code a bit

--- bibletools/parse_usfm.py
+++ bibletools/parse_usfm.py
@@ -30,7 +30,21 @@ def remove_footnotes(book, chapter, verse, text):
             assert False
     return out
 
-TOREMOVE = "\\nb \\p \\q2 \\q \\b".split()
+CROSSREF_PATTERN = re.compile(r"\\x .*?\\x[*]")
+START_CROSSREF_PATTERN = re.compile(r"\\x")
+def remove_crossrefs(book, chapter, verse, text):
+    """Heavy-handedly use a regex to remove cross-references. Returns a new
+    string."""
+    out = re.sub(CROSSREF_PATTERN, "", text)
+    if out == text:
+        if re.search(START_CROSSREF_PATTERN, text):
+            util.dprint("not good -- start of crossref but no end?")
+            util.dprint(text)
+            util.dprint(chapter, verse, text)
+            assert False
+    return out
+
+TOREMOVE = "\\wj* \\wj \\nb \\li \\p \\q2 \\q \\b \\m".split()
 def remove_formatting(book, chapter, verse, text):
     """Given the text of a verse, strip out the USFM formatting markers."""
     out = text
@@ -45,6 +59,7 @@ def remove_formatting(book, chapter, verse, text):
 
 def clean_and_print(book, chapter, verse, text):
     fixed = remove_footnotes(book, chapter, verse, text)
+    fixed = remove_crossrefs(book, chapter, verse, fixed)
     fixed = remove_formatting(book, chapter, verse, fixed)
     fixed = fixed.strip()
     if fixed:
@@ -61,30 +76,44 @@ def main():
         with open(bookfn) as infile:
             for line in infile:
                 line = line.strip()
+                # print("LINE", line)
                 if any(line.startswith(startmarker)
-                       for startmarker in ["\\v", "\\h", "\\c"]):
+                       for startmarker in ["\\v", "\\h", "\\c", "\\d"]):
                     if book and chapter and verse and text:
                         clean_and_print(book, chapter, verse, text)
                         text = ""
+                    if book and chapter and text and (verse is None):
+                        util.dprint("skipping structure information:", text)
+                    if line.startswith("\\h"):
+                        splitted = line.split(maxsplit=1)
+                        bookname = splitted[1]
+                        book = booknames.code(bookname)
+                        if not book:
+                            util.dprint("warning! not a known book:", bookname)
+                        else:
+                            seenbooks.add(book)
+                    elif line.startswith("\\c"):
+                        splitted = line.split()
+                        chapter = splitted[1]
+                    elif line.startswith("\\v"):
+                        splitted = line.split(maxsplit=2)
+                        verse = splitted[1]
+                        text = splitted[2]
+                    elif line.startswith("\\d"):
+                        splitted = line.split(maxsplit=1)
+                        verse = None
+                        text = splitted[1]
+                        continue
                 else:
                     text = text + " " + line
-                if line.startswith("\\h"):
-                    splitted = line.split(maxsplit=1)
-                    bookname = splitted[1]
-                    book = booknames.code(bookname)
-                    if not book:
-                        util.dprint("warning! not a known book:", bookname)
-                    else:
-                        seenbooks.add(book)
-                elif line.startswith("\\c"):
-                    splitted = line.split()
-                    chapter = splitted[1]
-                elif line.startswith("\\v"):
-                    splitted = line.split(maxsplit=2)
-                    verse = splitted[1]
-                    text = splitted[2]
+            ## hit the end of this file.
             if book and chapter and verse and text:
                 clean_and_print(book, chapter, verse, text)
+                book = None
+                chapter = None
+                verse = None
+                text = ""
+
 
     util.dprint("Saw this many books out of expected 66:",len(seenbooks))
     util.dprint("books we haven't seen:", booknames.knownbooks() - seenbooks)
--- bibletools/parse_usfm_test.py
+++ bibletools/parse_usfm_test.py
@@ -3,6 +3,9 @@
 verse_gen_4_1 = "The man knew\\f + or, lay with, or, had relations with\\f* Eve his wife. She conceived,\\f + or, became pregnant\\f* and gave birth to Cain, and said, “I have gotten a man with Yahweh’s help.”"
 verse_gen_4_1_nofootnote = "The man knew Eve his wife. She conceived, and gave birth to Cain, and said, “I have gotten a man with Yahweh’s help.”"
 
+verse_kgs_12_4 = "Jehoash said to the priests, “All the money of the holy things that is brought into Yahweh’s house, in current money, the money of the people for whom each man is evaluated,\\x + \\+zref \\+ztgt EXO.30.12 \\+ztgt*\\+zref*Exodus 30:12\\+zrefend \\+zrefend*\\x* and all the money that it comes into any man’s heart to bring into Yahweh’s house,"
+verse_kgs_12_4_nocrossref = "Jehoash said to the priests, “All the money of the holy things that is brought into Yahweh’s house, in current money, the money of the people for whom each man is evaluated, and all the money that it comes into any man’s heart to bring into Yahweh’s house,"
+
 import unittest
 
 import parse_usfm
@@ -17,6 +20,10 @@ class TestCleanup(unittest.TestCase):
         wrong."""
         cleaned = parse_usfm.remove_footnotes("GEN", 4, 1, verse_gen_4_1)
         self.assertEqual(cleaned, verse_gen_4_1_nofootnote)
+
+    def test_crossref_kgs_12_4(self):
+        cleaned = parse_usfm.remove_crossrefs("KGS", 12, 4, verse_kgs_12_4)
+        self.assertEqual(cleaned, verse_kgs_12_4_nocrossref)
 
 if __name__ == '__main__':
     unittest.main()
