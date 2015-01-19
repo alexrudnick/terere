43df94f533df65ab04e3cb8df81d9fcf23fc2da5
Alex Rudnick <alex.rudnick@gmail.com>
2015-01-19 14:38:42 -0800

    fixing and simplifying lemmatizer interface, since we'll only use it for gn and qu now

--- bibletools/lemmatize_text.py
+++ bibletools/lemmatize_text.py
@@ -19,73 +19,34 @@ sys.path.append(antimorfo)
 import l3
 import antimorfo
 
-LANGUAGES = "en gn qu".split()
-
-## MA for en
-import nltk
-wnl = nltk.stem.WordNetLemmatizer()
+LANGUAGES = "gn qu".split()
 
 def analyze(lang, word):
     """Return either a morphological analysis of the word, or if we can't do
-    that, just the surface form again. Also return what we think its pos tag
-    is. Only do this for qu and gn."""
-
-    assert lang in ["qu", "gn"] ## need to run pos tagger on English sentences
-
+    that, just the surface form again. Only do this for qu and gn."""
     ## XXX: speed hack: don't lemmatize ever.
-    ## return ("lemma:" + word, "thisisthetag")
-
-    analyses = []
+    ## return ("lemma:" + word)
     if "qu" == lang:
         analyses = antimorfo.anal_word("qu", word, raw=True)
     else:
         analyses = l3.anal(lang, word, raw=True)
-
     ## remove Nones from the list? ...
     analyses = [a for a in analyses if (a and a[0])]
-
     if not analyses:
         return word
     ## XXX: really we want morphological disambiguation here; is the first
     ## analysis the most likely? Probably they're unordered.
     lemmas = sorted(set(a[0] for a in analyses))
-
     postags = sorted(set(a[1]['pos'] for a in analyses))
-    print(postags)
-
+    ## print(postags)
     return "/".join(lemmas)
-
-VERBS = "MD VB VBD VBG VBN VBP VBZ".split()
-NOUNS = "NN NNS NNP NNPS".split()
-ADJECTIVES = "JJ JJR JJS".split()
 
 def analyze_sentence(lang, words):
     """Given a list of words (like a sentence or a bible verse), return the list
     of the lemmatized forms of those words."""
 
-    if "en" == lang:
-        tagged = nltk.pos_tag(words)
-        tags = [tag for word,tag in tagged]
-        lemmas = []
-        for (word, tag) in tagged:
-            wntag = None
-            if tag in VERBS:
-                wntag = "v"
-            elif tag in NOUNS:
-                wntag = "n"
-            elif tag in ADJECTIVES:
-                wntag = "a"
-            if wntag:
-                lemmas.append(wnl.lemmatize(word, wntag))
-            else:
-                lemmas.append(word)
-        assert len(words) == len(lemmas)
-        return lemmas
     ## not English, just run the MA
-    lemmas = []
-    for w in words:
-        lemma, tag = analyze(lang, w) 
-        lemmas.append(lemma)
+    lemmas = [analyze(lang, w) for w in words]
     return lemmas
 
 def main():
