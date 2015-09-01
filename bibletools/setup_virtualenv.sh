8a74031a3e8d8886d65040ef649e4a221b239885
Alex Rudnick <alex.rudnick@gmail.com>
2015-09-01 00:39:07 -0700

    update nltk in virtualenv

--- bibletools/setup_virtualenv.sh
+++ bibletools/setup_virtualenv.sh
@@ -4,5 +4,4 @@ virtualenv -p /usr/bin/python3 venv
 . venv/bin/activate
 pip install beautifulsoup4
 pip install numpy
-pip install https://github.com/nltk/nltk/tarball/develop
-
+pip install https://github.com/nltk/nltk/archive/3.0.4.zip
