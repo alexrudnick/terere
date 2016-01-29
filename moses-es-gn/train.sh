f04c6f3f29a8af415791d0dfcf13c0e293d42ea6
Alex Rudnick <alex.rudnick@gmail.com>
2016-01-29 14:18:50 -0800

    e and f are really less understandable than 'source' and 'target'

--- moses-es-gn/train.sh
+++ moses-es-gn/train.sh
@@ -16,7 +16,7 @@ cat ~/terere/bibletools/output/bible.es-gn \
   --cores 8 \
   --external-bin-dir /space/mosesdecoder/tools \
   --corpus ./corpus/bible \
-  --f gn \
-  --e es \
+  --f es \
+  --e gn \
   --alignment grow-diag-final-and \
   --lm 0:3:/space/terere/moses-es-gn/nc.klm
