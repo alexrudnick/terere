b542b9f259a96a0d74a5fa14d28fb3b61bc85740
Alex Rudnick <alex.rudnick@gmail.com>
2016-01-28 21:09:38 -0800

    trying to make phrase-based mt go rather than scfgs

--- cdec-es-gn/extract_pt.sh
+++ cdec-es-gn/extract_pt.sh
@@ -4,5 +4,9 @@ BITEXT=~/terere/bibletools/output/bible.es-gn
 head -n 100 $BITEXT > dev.lc-tok.es-gn
 head -n 200 $BITEXT | tail -n 100 > devtest.lc-tok.es-gn
 
-~/cdec/extractor/extract -c extract.ini -g dev.grammars --max_nonterminals 0 -t 2 -z < dev.lc-tok.es-gn > dev.lc-tok.es-gn.sgm
-~/cdec/extractor/extract -c extract.ini -g devtest.grammars --max_nonterminals 0 -t 2 -z < devtest.lc-tok.es-gn > devtest.lc-tok.es-gn.sgm
+~/cdec/extractor/extract -c extract.ini -g dev.grammars --max_nonterminals 0 -t 2 < dev.lc-tok.es-gn > dev.lc-tok.es-gn.sgm
+~/cdec/extractor/extract -c extract.ini -g devtest.grammars --max_nonterminals 0 -t 2 < devtest.lc-tok.es-gn > devtest.lc-tok.es-gn.sgm
+
+## turn grammars into phrase-based phrase tables
+sed -i "s/^\[X\] ||| //g" dev.grammars/*
+sed -i "s/^\[X\] ||| //g" devtest.grammars/*
--- cdec-es-gn/tune_mira.sh
+++
@@ -1,1 +0,0 @@
-~/cdec/training/mira/mira.py -d dev.lc-tok.es-gn.sgm -t devtest.lc-tok.es-gn.sgm -c cdec.ini -j 2
