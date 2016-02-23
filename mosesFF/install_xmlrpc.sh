2ef3e18de65873385e1597fad438fa7dd51445da
Alex Rudnick <alex.rudnick@gmail.com>
2016-02-22 23:51:11 -0800

    getting closer I think

--- mosesFF/install_xmlrpc.sh
+++ mosesFF/install_xmlrpc.sh
@@ -7,8 +7,19 @@ WORKDIR=$HOME/mosesdecoder/build
 MOSESOPT=$HOME/mosesdecoder/opt
 
 mkdir -p WORKDIR
-cd $WORKDIR
 
+## Install libcurl first, so we'll have that available when we build xmlrpc.
+cd $WORKDIR
+wget https://curl.haxx.se/download/curl-7.47.1.tar.gz
+tar xf curl-7.47.1.tar.gz
+cd curl-7.47.1
+./configure --prefix="$MOSESOPT"
+make
+make install
+
+
+## Now build xmlrpc.
+cd $WORKDIR
 REPOS=http://svn.code.sf.net/p/xmlrpc-c/code/advanced
 svn checkout $REPOS xmlrpc-c
 
@@ -18,10 +29,3 @@ make
 make install
 
 
-cd $WORKDIR
-wget https://curl.haxx.se/download/curl-7.47.1.tar.gz
-tar xf curl-7.47.1.tar.gz
-cd curl-7.47.1
-./configure --prefix="$MOSESOPT"
-make
-make install
