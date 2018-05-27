54cbaa90f02c24dafec112429d878d01bf8e33f9
Alex Rudnick <alex.rudnick@gmail.com>
2018-05-26 23:41:54 -0700

    simplify one_sentence_per_line because we're always using cdec tokenizer for gn

--- build-gn-lm/one_sentence_per_line.py
+++ build-gn-lm/one_sentence_per_line.py
@@ -3,9 +3,11 @@
 """
 Usage: python3 one_sentence_per_line.py infile.txt outfile.txt
 
-Take a text file, use NLTK to segment the sentences and tokenize the words (both
-in the default NLTK way), then print out one sentence per line with the words
-already tokenized, into the specified output file.
+Take a text file, use NLTK to segment the sentences and print out one sentence
+per line into the specified output file.
+
+For consistency here, do not use NLTK's word tokenizer; we're always using the
+cdec tokenizer for Guarani.
 """
 
 import argparse
@@ -13,12 +15,9 @@ import nltk
 
 def get_argparser():
     """Build the argument parser for main."""
-    parser = argparse.ArgumentParser(description='memmwsd')
+    parser = argparse.ArgumentParser(description='one_sentence_per_line')
     parser.add_argument('--infn', type=str, required=True)
     parser.add_argument('--outfn', type=str, required=True)
-    parser.add_argument('--tokenize',dest='tokenize',action='store_true')
-    parser.add_argument('--no-tokenize',dest='tokenize',action='store_false')
-    parser.set_defaults(tokenize=False)
     return parser
 
 def main():
@@ -26,17 +25,13 @@ def main():
     args = parser.parse_args()
 
     with open(args.infn) as infile:
-        text = infile.read()
-
-    sentences = nltk.sent_tokenize(text)
-    with open(args.outfn, "w") as outfile:
-        for sent in sentences:
-            sent = sent.strip()
-            if not sent: continue
-            if args.tokenize:
-                toks = nltk.word_tokenize(sent)
-                print(" ".join(toks), file=outfile)
-            else:
-                print(sent, file=outfile)
+        with open(args.outfn, "w") as outfile:
+            for line in infile:
+                line = line.strip()
+                sentences = nltk.sent_tokenize(line)
+                for sent in sentences:
+                    sent = sent.strip()
+                    if not sent: continue
+                    print(sent, file=outfile)
 
 if __name__ == "__main__": main()
