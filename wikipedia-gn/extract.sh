f39c436c81b2bcb3d0e67801f652de4b78c783cf
Alex Rudnick <alex.rudnick@gmail.com>
2018-05-26 23:33:55 -0700

    remove tags in gn wiki output

--- wikipedia-gn/extract.sh
+++ wikipedia-gn/extract.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 python WikiExtractor.py < gnwiki-latest-pages-articles.xml 
-cat AA/wiki_* > plaintext.txt
+
+# cat all the output wiki pages together and remove any remaining tags.
+cat AA/wiki_* \
+  | sed s/\<.*\>//g \
+  > plaintext.txt
