9be639a4e71bf1390c0ee240bbea47d745b56e9f
Alex Rudnick <alex.rudnick@gmail.com>
2018-05-27 22:51:12 -0700

    make LM corpus script for gn put one sentence on a line and lemmatize all the text

--- gn-lm-wikipedia/extract.sh
+++ gn-lm-wikipedia/extract.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-python WikiExtractor.py < gnwiki-latest-pages-articles.xml 
+python2 WikiExtractor.py < gnwiki-latest-pages-articles.xml 
 
 # cat all the output wiki pages together and remove any remaining tags.
 cat AA/wiki_* \
   | sed s/\<.*\>//g \
-  > plaintext.txt
+  > gnwiki-text.txt
+
+python3 one_sentence_per_line.py \
+  --infn gnwiki-text.txt \
+  --outfn gnwiki-text-sentences.txt
+
+python3 ../bibletools/lemmatize_text.py gn \
+  gnwiki-text-sentences.txt \
+  gnwiki-text-sentences-lemmatized.txt
