2eaf2a76496cc811ae5c7021843da82dc83f6eca
Alex Rudnick <alex.rudnick@gmail.com>
2018-05-28 22:46:04 -0700

    make sure to lowercase and tokenize before we run the gn lemmatizer on our LM text

--- gn-lm-wikipedia/extract.sh
+++ gn-lm-wikipedia/extract.sh
@@ -11,6 +11,11 @@ python3 one_sentence_per_line.py \
   --infn gnwiki-text.txt \
   --outfn gnwiki-text-sentences.txt
 
+cat gnwiki-text-sentences.txt \
+  | ~/cdec/corpus/lowercase.pl \
+  | ~/cdec/corpus/tokenize-anything.sh \
+  > gnwiki-text-sentences.tok
+
 python3 ../bibletools/lemmatize_text.py gn \
-  gnwiki-text-sentences.txt \
-  gnwiki-text-sentences-lemmatized.txt
+  gnwiki-text-sentences.tok \
+  gnwiki-text-sentences.tok.lemmas
