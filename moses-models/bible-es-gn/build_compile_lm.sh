8fe39c5a9b761122f1cc24fa374c38bce4835cb0
Alex Rudnick <alex.rudnick@gmail.com>
2018-05-28 22:48:22 -0700

    use Wikipedia data in our gn language model as well

--- moses-models/bible-es-gn/build_compile_lm.sh
+++ moses-models/bible-es-gn/build_compile_lm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-cat ~/terere/bibletools/output/bible.es-gn.target.lemmas | ~/cdec/klm/lm/builder/lmplz --order 3 > nc.lm
+cat ~/terere/bibletools/output/bible.es-gn.target.lemmas \
+  ~/terere/gn-lm-wikipedia/gnwiki-text-sentences.tok.lemmas \
+  | ~/cdec/klm/lm/builder/lmplz --order 3 > nc.lm
 
 ~/cdec/klm/lm/build_binary nc.lm nc.klm
