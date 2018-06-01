48cd6051a28bac7a93720804bbc7083d4818b0ad
Alex Rudnick <alex.rudnick@gmail.com>
2018-05-31 23:19:51 -0700

    change bleu calculations to sacrebleu

--- moses-models/bible-es-gn/translate_testset.sh
+++ moses-models/bible-es-gn/translate_testset.sh
@@ -8,6 +8,8 @@ echo "TRANSLATING THE BAD TEST SET"
   < testset_100.source \
   > OUTPUT
 
+## Requires sacrebleu, which you can get by going:
+## sudo pip3 install sacrebleu
+## https://github.com/awslabs/sockeye/tree/master/contrib/sacrebleu
 echo "CALCULATING THE BAD BLEU SCORE"
-~/mosesdecoder/scripts/generic/multi-bleu.perl ./testset_100.target \
-  < OUTPUT
+sacrebleu ./testset_100.target < OUTPUT
