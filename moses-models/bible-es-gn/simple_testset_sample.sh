c9cba06f3386ddec3f744414ba5d56cf7bf6435d
Alex Rudnick <alex.rudnick@gmail.com>
2018-06-18 00:00:18 -0700

    cut-corpus uses 1-based indexing apparently?

--- moses-models/bible-es-gn/simple_testset_sample.sh
+++ moses-models/bible-es-gn/simple_testset_sample.sh
@@ -8,9 +8,9 @@
 shuf corpus-together.tmp > corpus-together.shuffled
 
 head -n 100 corpus-together.shuffled \
-  | ~/cdec/corpus/cut-corpus.pl 0 \
+  | ~/cdec/corpus/cut-corpus.pl 1 \
   > bad-testset.source
 
 head -n 100 corpus-together.shuffled \
-  | ~/cdec/corpus/cut-corpus.pl 1 \
+  | ~/cdec/corpus/cut-corpus.pl 2 \
   > bad-testset.target
