3d1427fdcb322d30b59b72e1440d6b065c70f5be
Alex Rudnick <alex.rudnick@gmail.com>
2018-06-18 23:54:08 -0700

    looks like we're set to go with tokenization issues here, now to actually make lexical selection decisions

--- moses-models/bible-es-gn/translate_testset.sh
+++ moses-models/bible-es-gn/translate_testset.sh
@@ -5,11 +5,11 @@ echo "TRANSLATING THE BAD TEST SET"
 "$HOME"/mosesdecoder/bin/moses \
   --config model/moses.ini \
   --threads 1 \
-  < testset_100.source \
+  < bad-testset.source \
   > OUTPUT
 
 ## Requires sacrebleu, which you can get by going:
 ## sudo pip3 install sacrebleu
 ## https://github.com/awslabs/sockeye/tree/master/contrib/sacrebleu
 echo "CALCULATING THE BAD BLEU SCORE"
-sacrebleu ./testset_100.target < OUTPUT
+sacrebleu ./bad-testset.target < OUTPUT
